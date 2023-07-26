4b348fb648f03213a66d25a52dbc8409bca4feb3
Zach Gurevich <zachgurevich@gmail.com>
2023-07-26 16:19:07 -0500

    Delete scraps.py

--- scraps.py
+++
@@ -1,8 +0,0 @@
-prompt = f"""You will be provided with a question delimited by angle brackets \
-    output a python list of words or phrases that you do not understand from the question. \
-    If you understand all of the words and phrases output an empty python list.\
-    
-    question: <{question}>"""
-
-response = get_completion(prompt, MODEL)
-print(response)
